e0aa0cb06282ab567ecdfb9a7d4ead20ef852bdd
Danay4 <danayttesfu4@gmail.com>
2025-10-27 04:59:23 +0100

    p

--- public/index.php
+++ public/index.php
@@ -1,26 +1,25 @@
-﻿<?php $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/'); ?>
-<!doctype html>
+﻿<!doctype html>
 <html lang="no">
 <head>
   <meta charset="utf-8">
   <title>PRG120 – Vedlikeholdsapp</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
 <body>
   <h1>PRG120 – Vedlikeholdsapp</h1>
 
   <h2>Klasse</h2>
   <ul>
-    <li><a href="<?= $base ?>/klasse_list.php">Vis alle</a></li>
-    <li><a href="<?= $base ?>/klasse_add.php">Registrer ny</a></li>
-    <li><a href="<?= $base ?>/klasse_delete.php">Slett</a></li>
+    <li><a href="klasse_list.php">Vis alle</a></li>
+    <li><a href="klasse_add.php">Registrer ny</a></li>
+    <li><a href="klasse_delete.php">Slett</a></li>
   </ul>
 
   <h2>Student</h2>
   <ul>
-    <li><a href="<?= $base ?>/student_list.php">Vis alle</a></li>
-    <li><a href="<?= $base ?>/student_add.php">Registrer ny</a></li>
-    <li><a href="<?= $base ?>/student_delete.php">Slett</a></li>
+    <li><a href="student_list.php">Vis alle</a></li>
+    <li><a href="student_add.php">Registrer ny</a></li>
+    <li><a href="student_delete.php">Slett</a></li>
   </ul>
 </body>
 </html>
-
